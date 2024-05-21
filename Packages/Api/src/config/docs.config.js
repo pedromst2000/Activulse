972048c14f6646be8d856cdd205673666d2e0191
pedromst2000 <40210465@esmad.ipp.pt>
2024-05-22 00:38:37 +0100

    chores: added config files

---
+++ Packages/Api/src/config/docs.config.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const YAML = require("yaml");
+const routeDocs = require("../docs/routes");
+
+const info = YAML.parse(fs.readFileSync("./src/docs/info.yml", "utf8"));
+
+const serverURL =
+	process.env.NODE_ENV === "development"
+		? "http://localhost:5000/api/v1"
+		: process.env.API_PROD_URL;
+
+/** @type {import("swagger-jsdoc").SwaggerDefinition} */
+const swaggerDefinition = {
+	openapi: "3.0.1",
+	info,
+	servers: [{ url: serverURL }],
+	paths: {
+		// DEFAULT
+		"/": {
+			get: routeDocs.default.getHelloWorld,
+		},
+		"/{any*}": {
+			get: routeDocs.default.notFound,
+		},
+	},
+};
+
+module.exports = swaggerDefinition;
---
+++ Packages/Api/src/config/pagination.config.js
@@ -0,0 +1,32 @@
+const config = {
+	challenges: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 3,
+			maxLimit: 10,
+		},
+	},
+	activities: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 4,
+			maxLimit: 10,
+		},
+	},
+	recipes: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 3,
+			maxLimit: 10,
+		},
+	},
+	banners: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 4,
+			maxLimit: 10,
+		},
+	},
+};
+
+module.exports = config;
