911bb0713eb295708e3b1505d7e7707d3c82ad82
pedromst2000 <40210465@esmad.ipp.pt>
2024-08-21 02:06:10 +0100

    feat: Add LastSlideBtn Component

---
+++ Packages/App/src/components/Onboarding/LastSlideBtn.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+
+type Props = {
+	activeOpacity?: number;
+	text?: string;
+	className?: string;
+	onPress?: () => void | Promise<void>;
+} & React.ComponentProps<typeof TouchableOpacity>;
+
+const LastSlideButton: React.FC<Props> = ({
+	activeOpacity,
+	className,
+	onPress,
+	...props
+}): React.JSX.Element => {
+	return (
+		<TouchableOpacity
+			className={`flex flex-row items-center justify-center px-[45px] py-[10px] 
+                rounded-md  rounded-[30px] ${className}`}
+			activeOpacity={activeOpacity || 0.75}
+			onPress={() => {
+				console.log('Button pressed');
+				if (onPress) {
+					onPress();
+				}
+			}}
+			{...props}
+		>
+			<Text className="font-quicksand-bold text-secondary-700 text-base">{props.text}</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default LastSlideButton;
