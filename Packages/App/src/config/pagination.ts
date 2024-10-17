d38e3b78519378b3a95f167ae2f81622ccf0b0b1
pedromst2000 <40210465@esmad.ipp.pt>
2024-10-17 19:07:40 +0100

    refactor: updated pagination config file

--- Packages/App/src/config/pagination.ts
+++ Packages/App/src/config/pagination.ts
@@ -20,7 +20,30 @@ export default {
 			maxLimit: 10,
 		},
 	},
+	premium: {
+		activities: {
+			feed: {
+				defaultPage: 1,
+				defaultLimit: 3,
+				maxLimit: 6,
+			},
+		},
+		recipes: {
+			feed: {
+				defaultPage: 1,
+				defaultLimit: 3,
+				maxLimit: 6,
+			},
+		},
+	},
 	favorites: {
+		feed: {
+			defaultPage: 1,
+			defaultLimit: 4,
+			maxLimit: 10,
+		},
+	},
+	badges: {
 		feed: {
 			defaultPage: 1,
 			defaultLimit: 4,
